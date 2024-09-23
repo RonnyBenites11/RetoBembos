01cc265af37c12e2905ec78e2761c62412f57981
Ronny Benites <ronnybenitesrosado@gmail.com>
2024-09-23 17:58:11 -0500

    solucionando errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-<<<<<<< HEAD
+import React from 'react';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Carrusel from './components/Carrusel';
 /*import { useState } from 'react';*/
 import './App.css';
 import { ContainerProducts } from './components/ContainerProducts/ContainerProducts';
@@ -6,33 +9,21 @@ import { ContainerProducts } from './components/ContainerProducts/ContainerProdu
 export const App = () => {
   return (
     <div>
-      <ContainerProducts sectionName={'promocion'} />
-      <ContainerProducts sectionName={'menu'} />
-    </div>
-  );
-};
-=======
-import React from 'react';
-import Header from './components/Header';
-import './App.css';
-import Navigation from './components/Navigation';
-import Carrusel from './components/Carrusel';
-
-
-
-const App = () => {
-  return (
-    <div className="app">
       <div className='fix'>    
       <Header />  
       <Navigation/>
       </div>
       <Carrusel/>
-     
-      
+      <ContainerProducts sectionName={'promocion'} />
+      <ContainerProducts sectionName={'menu'} />
     </div>
   );
 };
 
-export default App;
->>>>>>> c5b8febb6ab0324fabe3799c7a7ce3723db76fc3
+
+
+
+
+
+
+
