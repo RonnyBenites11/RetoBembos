c5b8febb6ab0324fabe3799c7a7ce3723db76fc3
Ronny Benites <ronnybenitesrosado@gmail.com>
2024-09-23 16:43:49 -0500

    solucionando porblemas de fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Carrusel from './components/Carrusel';
 
+
+
 const App = () => {
   return (
     <div className="app">
       <div className='fix'>
+      <div>
       <Header />
+      </div>
+      <div>
       <Navigation/>
+      </div>
       </div>
       <Carrusel/>
      
