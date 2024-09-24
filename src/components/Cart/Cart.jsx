e5ce63706b6779835e543a8d1d31c50531227236
Christian Sotil <crsv0198@gmail.com>
2024-09-24 09:43:53 -0500

    estilos footer

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,13 +6,15 @@ export const Cart = () => {
     <div className="cart-container">
       <div className="cart-adress">
         <p className="cart-adress-title">
-          <span className="cart-adress-txt">Entregar en: Miraflores</span> Bowling Park 135,
+          <span className="cart-adress-txt">Entregar en: </span>Miraflores Bowling Park 135,
           <span className="cart-adress-city">Lima - Miraflores</span>
         </p>
-        <div>
-          <img src="" alt="" />
+        <div className="cart-down-img">
+          <img src="/src/assets/img/down-arrow.svg" alt="" />
         </div>
-        <span>x</span>
+        <div className="cart-close">
+          <span>x</span>
+        </div>
       </div>
     </div>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,56 +2,89 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="footer-container">
-                <div className="Icons">
-                    <a href="#"><i className="fa-brands fa-facebook"></i></a>
-                    <a href="#"><i className="fa-brands fa-youtube"></i></a>
-                    <a href="#"><i className="fa-brands fa-twitter"></i></a>
-                    <a href="#"><i className="fa-brands fa-instagram"></i></a>
-                </div>
+  return (
+    <footer>
+      <div className="footer-wrapper">
+        <div className="social-icons">
+          <a href="#">
+            <img src="/src/assets/img/Facebook.svg" alt="" />
+          </a>
 
-                <div className="footerNav">
-                    <ul>
-                        <li><a href="#">Locales</a></li>
-                        <li><a href="#">Zonas de Reparto</a></li>
-                        <li><a href="#">Nosotros</a></li>
-                        <li><a href="#">Preguntas Frecuentes</a></li>
-                        <li><a href="#">Mapa de Sitio</a></li>
-                        <li><a href="#">Política de Datos Personales</a></li>
-                        <li><a href="#">Términos y condiciones de Promociones</a></li>
-                        <li><a href="#">Derechos ARCO</a></li>
-                        <li><img src="./img/bembos logo.png" alt="Bembos Logo" id="bembos-logo" /></li>
-                    </ul>
-                </div>
+          <a href="#">
+            <img src="/src/assets/img/Youtube.svg" alt="" />
+          </a>
 
-                <div className="PayMethods">
-                    <p>FORMAS DE PAGO</p>
-                    <img src="/src/assets/img/visa1.png" alt="Visa" className="payment-logo" />
-                    <img src="/src/assets/img/mastercard3.png" alt="MasterCard" className="payment-logo" />
-                    <img src="/src/assets/img/AExpress4.png" alt="American Express" className="payment-logo" />
-                    <img src="/src/assets/img/" alt="SafetyPay" className="logoSpay" />
-                    <button className="btnComprobantes">COMPROBANTES</button>
-                </div>
+          <a href="#">
+            <img src="/src/assets/img/Twitter.svg" alt="" />
+          </a>
 
-                <div className="form">
-                    <p>¿Quiere recibir promociones y noticias?</p>
-                    <input type="text" placeholder="Tu Correo" />
-                    <button>Enviar</button>
-                    <div className="checkbox-wrapper">
-                        <input type="checkbox" id="terms" />
-                        <label htmlFor="terms" className="checkboxLabel">He leído y revisado los términos y condiciones</label>
-                    </div>
-                    <a href="#"><img src="/src/assets/img/pngegg.png" alt="Libro de Reclamaciones" className="bookImage" /></a>
-                </div>
+          <a href="#">
+            <img src="/src/assets/img/Instagram.svg" alt="" />
+          </a>
+        </div>
 
-                <div className="footerBottom">
-                    <p>© 2019 Todos los derechos reservados</p>
-                </div>
-            </div>
-        </footer>
-    );
+        <nav className="footer-links">
+          <ul>
+            <li>
+              <a href="#">Locales</a>
+            </li>
+            <li>
+              <a href="#">Zonas de Reparto</a>
+            </li>
+            <li>
+              <a href="#">Nosotros</a>
+            </li>
+            <li>
+              <a href="#">Preguntas Frecuentes</a>
+            </li>
+            <li>
+              <a href="#">Mapa de Sitio</a>
+            </li>
+            <li>
+              <a href="#">Política de Privacidad</a>
+            </li>
+            <li>
+              <a href="#">Términos y Condiciones</a>
+            </li>
+            <li>
+              <a href="#">Derechos de Usuario</a>
+            </li>
+            <li>
+              <img src="/src/assets/img/bembos.svg" alt="Logo Bembos" id="logo-bembos" />
+            </li>
+          </ul>
+        </nav>
+
+        <div className="payment-options">
+          <p>Formas de Pago</p>
+          <img src="/src/assets/img/visa1.png" alt="Visa" className="payment-img" />
+          <img src="/src/assets/img/mastercard3.png" alt="MasterCard" className="payment-img" />
+          <img src="/src/assets/img/AExpress4.png" alt="American Express" className="payment-img" />
+          <img src="/src/assets/img/pngegg.png" alt="SafetyPay" className="safetypay-img" />
+          <button className="receipt-button">Comprobantes</button>
+        </div>
+
+        <div className="newsletter">
+          <p>¿Te gustaría recibir nuestras promociones?</p>
+          <input type="email" placeholder="Tu Correo" />
+          <button>Suscribirse</button>
+          <div className="terms-checkbox">
+            <input type="checkbox" id="accept-terms" />
+            <label htmlFor="accept-terms" className="terms-label">
+              He leído y acepto los términos
+            </label>
+          </div>
+          <a href="#">
+            <img src="/src/assets/img/reclamaciones.jpg" alt="Libro de Reclamaciones" className="complaints-img" />
+          </a>
+        </div>
+
+        <div className="footer-bottom">
+          <p>&copy; 2019 Todos los derechos reservados</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
