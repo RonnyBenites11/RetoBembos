dd1004c503c58adb2b45a82f2613f6505f28c943
Andrew Maldonado <andrewmaldonado520@gmail.com>
2024-09-23 20:14:51 -0500

    Anuncio-Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,26 @@
-/*import { useState } from 'react';*/
-import './App.css';
-import { ContainerProducts } from './components/ContainerProducts/ContainerProducts';
-
-export const App = () => {
-  return (
-    <div>
-      <div className='fix'>    
-      <Header />  
-      <Navigation/>
-      </div>
-      <Carrusel/>
-      <ContainerProducts sectionName={'promocion'} />
-      <ContainerProducts sectionName={'menu'} />
-    </div>
-  );
-};
-
 import React from 'react';
-import Header from './components/Header';
 import './App.css';
+import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Carrusel from './components/Carrusel';
 import Anuncio from './components/Anuncio';
 import Footer from './components/Footer';
-
+import { ContainerProducts } from './components/ContainerProducts/ContainerProducts';
 
 const App = () => {
   return (
     <div className="app">
       <div className='fix'>
-      <div>
-      <Header />
+        <Header />
+        <Navigation/>
       </div>
-      <div>
-      <Navigation/>
-      </div>
-      </div>
-      <Carrusel/>
-      <div>
-      <Anuncio/>
-      </div>
-      <Footer/>
-      
+      <Carrusel />
+      <ContainerProducts sectionName={'promocion'} />
+      <ContainerProducts sectionName={'menu'} />
+      <Anuncio />
+      <Footer />
     </div>
   );
 };
 
 export default App;
-
