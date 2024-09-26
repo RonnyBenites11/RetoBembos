0ba4e034319819ef79e9a9db46f7e82f770f5136
Christian Sotil <crsv0198@gmail.com>
2024-09-26 09:47:18 -0500

    cambiando estilos al anuncio

--- src/components/Anuncio/Anuncio.jsx
+++ src/components/Anuncio/Anuncio.jsx
@@ -22,21 +22,20 @@ const Anuncio = () => {
             </div>
             <p className="shareLabel">Compartir en:</p>
 
-            <a href="https://x.com/bembosoficial?lang=es" target='_blank'>
-              <img src="/src/assets/img/twitter logo.png" width="145" height="32" alt="Twitter Bembos" loading="lazy" />
-            </a>
+            <div className="anuncio-socials">
+              <a href="https://x.com/bembosoficial?lang=es" target="_blank">
+                <img src="/src/assets/img/link-tw.webp" width="145" height="32" alt="Twitter Bembos" loading="lazy" />
+              </a>
 
-            <a href="https://www.facebook.com/bembos/" target='_blank'>
-              <img
-                src="/src/assets/img/facebook logo.png"
-                width="145"
-                height="32"
-                alt="Facebook Bembos"
-                loading="lazy"
-              />
-            </a>
+              <a href="https://www.facebook.com/bembos/" target="_blank">
+                <img src="/src/assets/img/link-fb.webp" width="145" height="32" alt="Facebook Bembos" loading="lazy" />
+              </a>
+            </div>
           </div>
           <div className="rightPanel">
+            <div className="iframe-icon">
+              <img src="/src/assets/img/icon-food.png" alt="" />
+            </div>
             <iframe
               src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbembos%2F&amp;tabs&amp;width=500&amp;height=130&amp;small_header=false&amp;adapt_container_width=true&amp;hide_cover=true&amp;show_facepile=true&amp;appId"
               width="500"
@@ -48,12 +47,9 @@ const Anuncio = () => {
               className="facebookEmbed"
             />
           </div>
-          </div>
-          
-         
-    </section>
+        </div>
+      </section>
     </div>
-
   );
 };
 
